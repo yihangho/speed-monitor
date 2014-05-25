b2503271c6db6bb2eebac5b24e8d587604b67d13
Yihang Ho <hoyihang5@gmail.com>
2014-05-25 11:43:05 +0800

    Able to specify a list of servers and finally fallback to auto mode.

--- cron/cron.rb
+++ cron/cron.rb
@@ -3,15 +3,31 @@ require 'net/http'
 require 'openssl'
 
 ts = Time.now.to_i
+if ENV["SPEED_MONITOR_SERVER_ID"]
+  servers = ENV["SPEED_MONITOR_SERVER_ID"].split(',').map { |x| x.strip }
+else
+  servers = []
+end
 
-speedtest_result_io =
-  if ENV["SPEED_MONITOR_SERVER_ID"]
-    IO.popen("/usr/local/bin/speedtest-cli --simple --server #{ENV["SPEED_MONITOR_SERVER_ID"]}")
-  else
-    IO.popen("/usr/local/bin/speedtest-cli --simple")
-  end
+server_used = nil
+results_arr = []
 
-results_arr = speedtest_result_io.read.scan(/\d+\.?\d*/)
+servers.each do |id|
+  next if id.empty?
+  puts "Trying #{id}"
+  speedtest_result_io = IO.popen("/usr/local/bin/speedtest-cli --simple --server #{id}")
+  results_arr = speedtest_result_io.read.scan(/\d+\.?\d*/)
+  server_used = id
+  break if results_arr.length >= 3
+end
+
+if results_arr.length < 3
+  server_used = "auto"
+  puts "Auto"
+  speedtest_result_io = IO.popen("/usr/local/bin/speedtest-cli --simple")
+  results_arr = speedtest_result_io.read.scan(/\d+\.?\d*/)
+end
+
 abort("Speed test failed.") unless results_arr.length >= 3
 ping, dl, ul = results_arr
 
@@ -27,7 +43,7 @@ else
   uri = URI('https://www.example.com/speed-test/api/submit.php')
 end
 
-puts "Ts: #{ts}, ping: #{ping}, dl: #{dl}, ul: #{ul}"
+puts "Ts: #{ts}, ping: #{ping}, dl: #{dl}, ul: #{ul}, id: #{server_used}"
 
 if uri.path.empty?
   uri.path = "/"
@@ -37,7 +53,7 @@ http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = (uri.scheme == "https")
 
 begin
-  response = http.request_post(uri.path, "timestamp=#{ts}&ping=#{ping}&dl=#{dl}&ul=#{ul}&key=#{key}")
+  response = http.request_post(uri.path, "timestamp=#{ts}&ping=#{ping}&dl=#{dl}&ul=#{ul}&key=#{key}&server=#{server_used}")
   p response
   puts response.body
 rescue Exception => e
