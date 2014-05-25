f60121f26bc900ca1b2d6d4592f0bff378307368
Yihang Ho <hoyihang5@gmail.com>
2014-05-25 18:42:13 +0800

    Update schema.

--- www/schema.sql
+++ www/schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS `speedtest` (
   `ping` float NOT NULL,
   `dl` float NOT NULL,
   `ul` float NOT NULL,
+  `server_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
