33885201c457643fd71d57f50b303dabf0aacf60
Yihang Ho <hoyihang5@gmail.com>
2014-05-26 10:12:15 +0800

    Create system table.

--- www/schema.sql
+++ www/schema.sql
@@ -8,3 +8,13 @@ CREATE TABLE IF NOT EXISTS `speedtest` (
   PRIMARY KEY (`id`),
   KEY `ts` (`ts`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+
+CREATE TABLE IF NOT EXISTS `system` (
+  `config` varchar(255) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `config` (`config`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `system` (`config`, `value`) VALUES
+('last_low_speed_notification', '0'),
+('last_no_submission_notification', '0');
